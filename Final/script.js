9d7cec0200f3ade8f75e70eab040b9c393bdecd0
Anna <paestum1@gmail.com>
2016-09-16 11:35:20 +0200

    Ajout copie du projet final

---
+++ Final/script.js
@@ -0,0 +1,11 @@
+
+
+function animatecur(i) {
+  document.getElementsByClassName("listcontent")[i].style.height = "200px" ;
+  console.log("something");
+}
+function animateremove(){
+  document.getElementsByClassName("listcontent")[0].style.height = "0px" ;
+  document.getElementsByClassName("listcontent")[1].style.height = "0px" ;
+
+}
